867b60c7d8c5a1c2c281971e779431379523adea
Thomas-Gallusser <t.gallusser@codeur.online>
2019-10-01 23:33:42 +0200

    tuto collision

--- collision/script.js
+++ collision/script.js
@@ -1,3 +1,9 @@
+// On reprend le tuto déplacement !
+// Petit changement sur les déplacements pour facilement gérer les collisions
+// + fonction toute prête de collisions à la fin !
+
+var taille_mouvement = 25;
+
 // Je commence à créer mon terrain, une simple div de taille 275x275
 var terrain = document.createElement("div");
 terrain.style.width = "275px";
@@ -43,22 +49,54 @@ terrain.appendChild(mur); // Je l'ajoute à mon body
 
 // On lit les enfoncements du clavier (sur "document" qui est la page)
 document.addEventListener("keydown", event => {
-  //console.log(event); // on peut log "event" pour voir tout ce qui peut être récupérer
-  //On peut soit récupérer le keyCode (exemple: 32) sois le code (exemple: "Space")
-  if (event.code == "ArrowUp") { // Si j'appuis sur flèche haut ..
-    player.style.top = (player.offsetTop - player.offsetHeight) + 'px'; // On ajoute à la position y du joueur sa hauteur
-  }
-  if (event.code == "ArrowDown") { // Si j'appuis sur flèche bas ..
-    player.style.top = (player.offsetTop - player.offsetHeight) + 'px'; // On retire à la position y du joueur sa hauteur
-  }
-  if (event.code == "ArrowRight") { // Si j'appuis sur flèche droite ..
-    player.style.left = (player.offsetLeft + player.offsetWidth) + 'px'; // On ajoute à la position x du joueur sa largeur
-  }
-  if (event.code == "ArrowLeft") { // Si j'appuis sur flèche gauche ..
-    player.style.left = (player.offsetLeft - player.offsetWidth) + 'px'; // On ajoute à la position x du joueur sa largeur
+  // On charge les valeurs avant
+  var nouvelle_valeur_x = player.offsetLeft;
+  var nouvelle_valeur_y = player.offsetTop;
+
+  if (event.code == "ArrowUp") nouvelle_valeur_y -= taille_mouvement;
+  if (event.code == "ArrowDown") nouvelle_valeur_y += taille_mouvement;
+  if (event.code == "ArrowRight") nouvelle_valeur_x += taille_mouvement;
+  if (event.code == "ArrowLeft") nouvelle_valeur_x -= taille_mouvement;
+
+  // Pour déplacer directement le joueur, n'est pas une bonne solution pour du cases par cases
+  if (!verifCollision(player,nouvelle_valeur_x,nouvelle_valeur_y,mur)) {
+    player.style.left = nouvelle_valeur_x + 'px';
+    player.style.top = nouvelle_valeur_y + 'px';
   }
 }, false); // Le False est important pour lire les flèches du clavier
 
 // Pour changer la position avec le style.top / style.left il faut que ce sois en 'px' obligatoirement
 // Quand on mélange calcul et texte il faut mettre le calcul entre parenthèse
 // (0 + 0) + ' = la tête à toto'
+
+// J'utilise 2 valeurs de déplacement pour calculer les futurs déplacements (sinon on calcul trop tard)
+function verifCollision(premier_objet,deplacement_en_x, deplacement_en_y, deuxieme_objet) {
+  if (deplacement_en_x < deuxieme_objet.offsetLeft + deuxieme_objet.offsetWidth &&
+     deplacement_en_x + premier_objet.offsetWidth > deuxieme_objet.offsetLeft &&
+     deplacement_en_y < deuxieme_objet.offsetTop + deuxieme_objet.offsetHeight &&
+     premier_objet.offsetHeight + deplacement_en_y > deuxieme_objet.offsetTop) {
+       return true; // collision
+  }
+  return false; // pas de collision
+}
+
+// Pour aller plus loin sur la collision en français lignes par lignes le if:
+
+// Si le bord gauche du premier_objet est inférieur au bord droite du deuxieme_objet ET SI
+// Si le bord droite du premier_objet est supérieur au bord gauche du deuxieme_objet ET SI
+// Si le bord haut du premier_objet est inférieur au bord bas du deuxieme_objet ET SI
+// Si le bord bas du premier_objet est supérieur au bord haut du deuxieme_objet
+// Alors on à une collision
+
+
+//-------------------------------------------------------------------------------------------------------------
+// C'est plus simple si on regarde qu'une direction, exemple si on regarde que la gauche et la droite:
+
+// |player|
+//     |mur|
+
+// Si on ne regarde pas la hauteur
+// le bord gauche du joueur est inférieur au bord droit du mur ET
+// le bord droit du joueur est supérieur au bord gauche du mur
+// Donc on est dedans si on regarde juste droite et gauche
+// on ne le serait pas si on regarde la hauteur
