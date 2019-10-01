2e234ab33dd77266a5846a4cf1217cdf2b4d25a1
Thomas-Gallusser <t.gallusser@codeur.online>
2019-10-01 20:31:54 +0200

    tuto repetitions

--- repetition/script.js
+++ repetition/script.js
@@ -1,23 +1,17 @@
 // La fonction "setTimeout" permet d'éxecuter une fonction au bout de X temps:
 var retour_de_la_balise_p;
 
-setTimeout(function() { // début de la fonction de mon setTimeout
-  retour_de_la_balise_p = document.createElement("p");
-  retour_de_la_balise_p.innerHTML = 'Ce texte s\'affichera au bout de 2 secondes';
-  document.body.appendChild(retour_de_la_balise_p);
-}, 2000); // fermeture de ma fonction avec en seconde paramètre un temps en milliseconde (1000 = 1 seconde)
 
-
-// Il est également possible de le faire plusieurs fois
-setTimeout(function() {
-  retour_de_la_balise_p = document.createElement("p");
-  retour_de_la_balise_p.innerHTML = 'Ce texte s\'affichera au bout de 3 secondes';
-  document.body.appendChild(retour_de_la_balise_p);
-
-  // Je relance ma fonction setTimeout dans ma fonction setTimeout !
-  setTimeout(function() {
+var ma_boucle = setInterval(repetition_toutes_les_2_secondes, 2000); // je créer une interval toutes les 2 secondes
+function repetition_toutes_les_2_secondes(){
     retour_de_la_balise_p = document.createElement("p");
-    retour_de_la_balise_p.innerHTML = 'Ce texte s\'affichera au bout de 4 secondes';
+    retour_de_la_balise_p.innerHTML = 'Ce texte s\'ajoutera toutes les 2 secondes';
     document.body.appendChild(retour_de_la_balise_p);
-  }, 1000); // Ma deuxième fonction setTimeout sera lancer 1 secondes après la précédente, 3 + 1 = 4 secondes
-}, 3000);
+}
+
+// J'arrête ma boucle après 6 secondes:
+setTimeout(function() {
+  clearInterval(ma_boucle); // On peut arrêter un setInterval grâce à la fonction "clearInterval";
+}, 6000);
+
+// Mon texte ne s'affichera que 3 fois
